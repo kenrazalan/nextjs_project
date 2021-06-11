d805c0001dcf3a7216176ac6cad1b2d7ad8c1d5e
kenrazalan <kennethrazalan29@gmail.com>
2021-06-11 15:12:05 +0800

    key feature

--- src/components/feature-card-column.js
+++ src/components/feature-card-column.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import Head from 'next/head';
 import { jsx, Image, Box, Heading, Text } from 'theme-ui';
 
 export default function FeatureCardColumn({
@@ -8,7 +9,15 @@ export default function FeatureCardColumn({
   text,
 }) {
   return (
-    <h1>FeatureCardColumn</h1>
+    <Box sx={styles.card}>
+      <Image src={src} altText={altText} sx={styles.img}/>
+      <Box sx={styles.wrapper.title}>
+        <Heading sx={styles.wrapper.title}>
+          {title}
+        </Heading>
+        <Text sx={styles.wrapper.subTitle}> {text} </Text>
+      </Box>
+    </Box>
   );
 }
 
